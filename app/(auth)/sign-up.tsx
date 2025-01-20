a19a5aa4d320fa9842f3e59cb62a073dc8101901
Karan Chopra <114980007+karanchopra1996@users.noreply.github.com>
2025-01-19 22:33:35 -0800

    Added Tabs, Ride card, and setup db
    - Added tabs: Home, chat, history and profile.
    - Connected the application to the DB, successfully tested the user creation in DB during a new user account creation.
    - Built the UI of the ride card component, displaying date, time, drive name, car seta, payment status.
    -Utilized Geoapify to generate the longitude and latitude of an address.

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -8,6 +8,7 @@ import CustomButton from "@/components/CustomButton";
 import InputField from "@/components/InputField";
 import OAuth from "@/components/OAuth";
 import { icons, images } from "@/constants";
+import { fetchAPI } from "@/lib/fetch";
 
 const SignUp = () => {
   const { isLoaded, signUp, setActive } = useSignUp();
@@ -50,6 +51,14 @@ const SignUp = () => {
         code: verification.code,
       });
       if (completeSignUp.status === "complete") {
+        await fetchAPI("/(api)/user", {
+          method: "POST",
+          body: JSON.stringify({
+            name: form.name,
+            email: form.email,
+            clerkId: completeSignUp.createdUserId,
+          }),
+        });
         await setActive({ session: completeSignUp.createdSessionId });
         setVerification({
           ...verification,
@@ -174,7 +183,10 @@ const SignUp = () => {
             </Text>
             <CustomButton
               title="Browse Home"
-              onPress={() => router.push(`/(root)/(tabs)/home`)}
+              onPress={() => {
+                setShowSuccessModal(false);
+                router.push(`/(root)/(tabs)/home`);
+              }}
               className="mt-5"
             />
           </View>
